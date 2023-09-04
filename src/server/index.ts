d11e4927a7017d0a017f7f2be37f5b3b7aa43617
Andrea Prat <andr8ide@gmail.com>
2023-09-04 16:08:44 +0200

    Create endpointNotFound middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,16 +2,22 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { endpointNotFound, generalError } from "./middlewares/errors.js";
+
+const corsOptions = {
+  origin: [process.env.ORIGIN_PROD!, process.env.ORIGIN_LOCAL!],
+};
 
 const app = express();
+
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 
-app.use(
-  cors({
-    origin: [process.env.ORIGIN_PROD!, process.env.ORIGIN_LOCAL!],
-  }),
-);
+app.use(cors(corsOptions));
+
+app.use(endpointNotFound);
+
+app.use(generalError);
 
 export default app;
