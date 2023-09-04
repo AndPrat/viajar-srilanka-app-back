3ff90856684d426361f8e03217fede4cbc6ce5bf
Andrea Prat <andr8ide@gmail.com>
2023-09-04 14:08:07 +0200

    Add cors configuration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,17 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN_PROD!, process.env.ORIGIN_LOCAL!],
+  }),
+);
 
 export default app;
