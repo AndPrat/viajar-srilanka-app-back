5a4ae0a5be35c6c53a7d7c350177444ba40657cc
Andrea Prat <andr8ide@gmail.com>
2023-09-05 10:34:18 +0200

    Create getPlaces controller

---
+++ src/server/controllers/placesControllers.ts
@@ -0,0 +1,22 @@
+import { type NextFunction, type Request, type Response } from "express";
+import Place from "../../database/models/Place.js";
+import CustomError from "../../CustomError/CustomError.js";
+
+export const getPlaces = async (
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const places = await Place.find().exec();
+    res.status(200).json({ places });
+  } catch (error: unknown) {
+    const customError = new CustomError(
+      "Can't retrieve the places",
+      500,
+      (error as Error).message,
+    );
+
+    next(customError);
+  }
+};
