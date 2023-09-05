e9428bb7452483a9da0116f750a0005e8c636a58
Andrea Prat <andr8ide@gmail.com>
2023-09-05 09:01:51 +0200

    Create Place model

--- src/database/models/types.ts
+++ src/database/models/types.ts
@@ -1,3 +1,15 @@
+export interface PlaceStructure {
+  _id: string;
+  name: string;
+  subtitle: string;
+  location: string;
+  schedule: string;
+  otherRelatedPlace: string;
+  description: string;
+  image: string;
+  user: string;
+}
+
 export interface UserStructure {
   _id: string;
   name: string;
