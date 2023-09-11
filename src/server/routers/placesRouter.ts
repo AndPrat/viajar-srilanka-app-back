c172b4f2d81e5daf5700c117f608ff143be6a1b4
Andrea Prat <andr8ide@gmail.com>
2023-09-11 18:37:05 +0200

    Fix placesRouter get method

--- src/server/routers/placesRouter.ts
+++ src/server/routers/placesRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import paths from "./paths/paths.js";
+import { getPlaces } from "../controllers/placesControllers/placesControllers.js";
 
 const placesRouter = express.Router();
 
-placesRouter.get(paths.root, placesRouter);
+placesRouter.get(paths.root, getPlaces);
 
 export default placesRouter;
