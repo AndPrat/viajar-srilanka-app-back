baf8eb1dad734845e5173dd8e4b7df2af3474da0
Andrea Prat <andr8ide@gmail.com>
2023-09-15 11:18:18 +0200

    Create addPlace controller and endpoint POST

--- src/server/routers/placesRouter.ts
+++ src/server/routers/placesRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import paths from "./paths/paths.js";
 import {
+  addPlace,
   deletePlaceById,
   getPlaces,
 } from "../controllers/placesControllers/placesControllers.js";
@@ -9,5 +10,6 @@ const placesRouter = express.Router();
 
 placesRouter.get(paths.root, getPlaces);
 placesRouter.delete(paths.deletePlaces, deletePlaceById);
+placesRouter.post(paths.root, addPlace);
 
 export default placesRouter;
